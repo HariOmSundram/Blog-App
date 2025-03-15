40cb14871339056e2c2d9deced21c339c469c09f
Hari Om Sundram <75016666+HariOmSundram@users.noreply.github.com>
2025-03-15 12:43:27 +0530

    Blog App

---
+++ app.js
@@ -0,0 +1,96 @@
+// Todo Usual init
+import express from "express";
+import bodyParser  from "body-parser";
+const app = express();
+const port = 3000;
+
+// Data Center
+let posts = [];
+
+
+// Post Constructor
+function Post(title, content) {
+    this.title = title;
+    this.content = content;
+    this.rawDate = new Date();
+    this.date = this.rawDate.toLocaleString();
+}
+
+// Add Post
+function addPost(title, content) {
+    let post = new Post(title, content);
+    posts.push(post);
+}
+
+// Delete Post
+function deletePost(index) {
+    posts.splice(index, 1);
+}
+// Edit Post
+function editPost(index, title, content) {
+    posts[index] = new Post(title, content);
+}
+
+// Midleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// Todo All paths
+
+// Home
+app.get("/", (req, res) => {
+    res.render("home.ejs", {posts: posts});
+});
+
+// View Post
+app.get("/view/:id", (req, res) => {
+    let index = req.params.id;
+    let post = posts[index];
+    res.render("view.ejs", {postId: index, title: post.title, content: post.content});
+});
+
+// Delete Post
+app.post("/delete", (req, res) => {
+    let index = req.body["postId"];
+    deletePost(index);
+    res.redirect("/");
+});
+
+// Edit Post Page
+app.get("/edit/:id", (req, res) => {
+    let index = req.params.id;
+    let post = posts[index];
+    res.render("create.ejs", {postId: index, title: post.title, content: post.content});
+});
+
+// Update
+app.post("/update", (req, res) => {
+    let title = req.body["title"];
+    let content = req.body["content"];
+    let index = req.body["index"];
+    editPost(index, title, content);
+    res.redirect("/");
+});
+
+// Create Post Page
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+// Save Post
+app.post("/save", (req, res) => {
+    let title = req.body["title"];
+    let content = req.body["content"];
+    
+    addPost(title, content);
+    res.redirect("/");
+});
+
+
+// Todo Listen thing
+app.listen(port, () => {
+    console.log(`Breaking News: Our app is all ears at Port ${port}, ready to boogie to the sweet sounds of data! 🎵🕺😄`)
+})
+
+
